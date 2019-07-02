2aa147490847d1d5fa3e12316b6b1b5cb2f49759
Darryl Banks <darryl.banks@zombie9.com>
2019-07-02 17:01:02 +0100

    add HttpClient & Forms modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -28,6 +31,8 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
   entryComponents: [],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     BrowserAnimationsModule,
